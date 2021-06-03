9a98fde1bb4e23ca6a230dcf61ce33daad0bf962
VoletiAnjula <85270327+VoletiAnjula@users.noreply.github.com>
2021-06-03 12:01:19 +0530

    MVC

--- StudentsController.cs
+++ StudentsController.cs
@@ -1,133 +1,155 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using System.Web.Http.Description;
-using SchoolWebApiAssignment;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using StudentsClient.Models;
 
-namespace SchoolWebApiAssignment.Controllers
+namespace StudentsClient.Controllers
 {
-    public class StudentsController : ApiController
+    public class StudentsController : Controller
     {
-        private SchoolDBEntities db = new SchoolDBEntities();
-
-        // GET: api/Students
-        public IQueryable<Student> GetStudents()
+        // GET: Students
+        public ActionResult Index()
         {
-            return db.Students;
+
+            IEnumerable<stud> supdata = null;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44392/api/";
+
+                var json = webClient.DownloadString("Students");
+                var list = JsonConvert.DeserializeObject<List<stud>>(json);
+                supdata = list.ToList();
+                return View(supdata);
+            }
         }
 
-        // GET: api/Students/5
-        [ResponseType(typeof(Student))]
-        public IHttpActionResult GetStudent(int id)
+        // GET: Students/Details/5
+        public ActionResult Details(int id)
         {
-            Student student = db.Students.Find(id);
-            if (student == null)
-            {
-                return NotFound();
-            }
 
-            return Ok(student);
+            stud supdata;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44392/api/";
+
+                var json = webClient.DownloadString("Students/" + id);
+                //  var list = emp 
+                supdata = JsonConvert.DeserializeObject<stud>(json);
+            }
+            return View(supdata);
         }
 
-        // PUT: api/Students/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutStudent(int id, Student student)
+        // GET: Students/Create
+        public ActionResult Create()
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            return View();
+        }
 
-            if (id != student.Rollno)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(student).State = EntityState.Modified;
+        // POST: Students/Create
+        [HttpPost]
+        public ActionResult Create(stud model)
+        {
 
             try
             {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!StudentExists(id))
+                using (WebClient webClient = new WebClient())
                 {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+                    webClient.BaseAddress = "https://localhost:44392/api/";
+                    var url = "Students/POST";
+                    //webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string data = JsonConvert.SerializeObject(model);
+                    var response = webClient.UploadString(url, data);
+                    JsonConvert.DeserializeObject<stud>(response);
 
-            return StatusCode(HttpStatusCode.NoContent);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
         }
 
-        // POST: api/Students
-        [ResponseType(typeof(Student))]
-        public IHttpActionResult PostStudent(Student student)
+        // GET: Students/Edit/5
+        public ActionResult Edit(int id)
         {
-            if (!ModelState.IsValid)
+            stud empdata;
+            using (WebClient webClient = new WebClient())
             {
-                return BadRequest(ModelState);
-            }
+                webClient.BaseAddress = "https://localhost:44392/api/";
 
-            db.Students.Add(student);
+                var json = webClient.DownloadString("Students/" + id);
+                //  var list = emp 
+                empdata = JsonConvert.DeserializeObject<stud>(json);
+            }
+            return View(empdata);
+        }
+
+        // POST: Students/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, stud model)
+        {
 
             try
             {
-                db.SaveChanges();
-            }
-            catch (DbUpdateException)
-            {
-                if (StudentExists(student.Rollno))
+                using (WebClient webClient = new WebClient())
                 {
-                    return Conflict();
+                    webClient.BaseAddress = "https://localhost:44392/api/Students/" + id;
+                    //var url = "Values/Put/" + id;
+                    //webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string data = JsonConvert.SerializeObject(model);
+                    var response = webClient.UploadString(webClient.BaseAddress, "PUT", data);
+                    stud modeldata = JsonConvert.DeserializeObject<stud>(response);
+
                 }
-                else
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int id)
+        {
+            stud supdata;
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.BaseAddress = "https://localhost:44392/api/";
+                var json = webClient.DownloadString("Students/" + id);
+                supdata = JsonConvert.DeserializeObject<stud>(json);
+            }
+            return View(supdata);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, stud model)
+        {
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
                 {
-                    throw;
+                    NameValueCollection nv = new NameValueCollection();
+                    string url = "https://localhost:44392/api/Students/" + id;
+                    var response = Encoding.ASCII.GetString(webClient.UploadValues(url, "DELETE", nv));
                 }
+
             }
-
-            return CreatedAtRoute("DefaultApi", new { id = student.Rollno }, student);
-        }
-
-        // DELETE: api/Students/5
-        [ResponseType(typeof(Student))]
-        public IHttpActionResult DeleteStudent(int id)
-        {
-            Student student = db.Students.Find(id);
-            if (student == null)
+            catch (Exception ex)
             {
-                return NotFound();
+                throw ex;
             }
-
-            db.Students.Remove(student);
-            db.SaveChanges();
-
-            return Ok(student);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool StudentExists(int id)
-        {
-            return db.Students.Count(e => e.Rollno == id) > 0;
+            return RedirectToAction("Index");
         }
     }
 }
